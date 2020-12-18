33e5cda4f41ddcf52b93ca177fb604f0e5b43bd5
JessicaThi <jessica.thielemans@hetic.net>
2020-12-19 00:18:27 +0100

    scss part 1

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import './scss/styles.scss';
+import adventure from "./img/adventure.svg"
 
 function App() {
   return (
-    <div className="App">
-      <p className="regular">Coucou</p>
-      <p className="medium">Coucou</p>
-      <p className="semibold">Coucou</p>
-      <p className="bold">Coucou</p>
-    </div>
+    <main className="App">
+      <div className="box">
+        <h1 className="title">Country Quizz</h1>
+        <div className="box-content">
+          <img className="box-content__image-decoration" src={adventure} alt="adventure" />
+          <p className="box-content__question">Kuala Lumpur is the capital of</p>
+          <article className="box-content__answer box-content__answer_wrong">A Vietnam</article>
+          <article className="box-content__answer box-content__answer_good">A Vietnam</article>
+          <article className="box-content__answer">A Vietnam</article>
+          <div className="validation">
+            <button className="button__full">Next</button>
+          </div>
+        </div>
+      </div>
+    </main>
   );
 }
 
