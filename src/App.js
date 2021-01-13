c4ffe7b4cd3df8e9d73810756ae28c635583f076
JessicaThi <jessica.thielemans@hetic.net>
2021-01-13 12:56:31 +0100

    add first call API [test]

--- src/App.js
+++ src/App.js
@@ -4,12 +4,21 @@ import adventure from "./img/adventure.svg"
 
 function App() {
   const [data, setData] = useState('');
+  const [randomNumber, setRandomNumber] = useState('');
 
   useEffect(() => {
-    fetch("https://restcountries.eu/rest/v2/name/belgium")
+    fetch("https://restcountries.eu/rest/v2/all")
       .then(response => response.json())
       .then(json => setData(json));
   }, []);
+
+  useEffect(() => {
+    if (data) {
+      setRandomNumber(Math.floor(Math.random() * (data.length - 0)) + 0);
+    }
+  }, [data])
+
+  console.log(randomNumber)
 
   return (
     <main className="App">
@@ -17,9 +26,9 @@ function App() {
         <h1 className="title">Country Quizz</h1>
         <div className="box-content">
           <img className="box-content__image-decoration" src={adventure} alt="adventure" />
-          {data ? <p className="box-content__question">{data[0].capital} is the capital of</p> : ''}
+          {randomNumber ? <p className="box-content__question">{data[randomNumber].capital} is the capital of {randomNumber}</p> : ''}
           <div className="box-content__answer box-content__answer_wrong">A Vietnam</div>
-          <div className="box-content__answer box-content__answer_good">B {data[0].name}</div>
+          {randomNumber ? <div className="box-content__answer box-content__answer_good">B {data[randomNumber].name}</div> : ''}
           <div className="box-content__answer">A Vietnam</div>
           <div className="validation">
             <button className="button__full">Next</button>
