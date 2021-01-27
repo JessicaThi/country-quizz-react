c7e7618c3edf819449d491e89eb89b2e7235be0b
JessicaThi <jessica.thielemans@hetic.net>
2021-01-27 17:57:47 +0100

    display right answer & add motion

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import { useState, useEffect } from "react"
 import './scss/styles.scss'
 import Questionaire from './components/Questionaire';
 import Result from "./components/Result"
+import { motion, AnimatePresence } from "framer-motion"
 
 const API_URL = 'https://restcountries.eu/rest/v2/all'
 
 function App() {
   const [data, setData] = useState('');
   const [randomAnswers, setRandomAnswers] = useState([]);
-  const [correctAnswerIndex, setCorrectAnswerIndex] = useState(null);
+  const [correctAnswerId, setCorrectAnswerId] = useState(null);
   const [selectedAnswerIndex, setSelectedAnswerIndex] = useState(null);
   const [nextStep, setNextStep] = useState('');
   const [counter, setCounter] = useState(0);
@@ -31,16 +32,12 @@ function App() {
         }
 
         if (randomAnswers.length === 4) {
-          setCorrectAnswerIndex(randomAnswers[Math.floor(Math.random() * (4 - 0)) + 0])
+          setCorrectAnswerId(randomAnswers[Math.floor(Math.random() * (4 - 0)) + 0])
         }
       }
       setRandomAnswers(randomAnswers);
     }
   }, [data, randomAnswers])
-
-  const chooseAnswer = (index) => {
-    setSelectedAnswerIndex(index)
-  }
 
   const checkAnswer = (answeredRight, index) => {
     setSelectedAnswerIndex(index);
@@ -53,7 +50,7 @@ function App() {
 
   const newQuestion = () => {
     setRandomAnswers([]);
-    setCorrectAnswerIndex(null);
+    setCorrectAnswerId(null);
     setSelectedAnswerIndex(null);
     setNextStep('');
   }
@@ -64,7 +61,7 @@ function App() {
 
   const restart = () => {
     setRandomAnswers([]);
-    setCorrectAnswerIndex(null);
+    setCorrectAnswerId(null);
     setSelectedAnswerIndex(null);
     setNextStep('');
     setCounter(0);
@@ -74,24 +71,35 @@ function App() {
   return (
     <main className="App">
       <div className="box">
-        <h1 className="title">Country Quizz</h1>
-        {(function () {
-          if (!myResult) {
-            return <Questionaire 
-                    data={data} 
-                    randomAnswers={randomAnswers} 
-                    correctAnswerIndex={correctAnswerIndex} 
-                    selectedAnswerIndex={selectedAnswerIndex}
-                    chooseAnswer={chooseAnswer} 
-                    checkAnswer={checkAnswer} 
-                    nextStep={nextStep}
-                    seeResult={seeResult}
-                    newQuestion={newQuestion} 
-                  />
-          } else {
-            return <Result counter={counter} restart={restart} />
+        <AnimatePresence key={myResult} exitBeforeEnter>
+          {!!randomAnswers.length && <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }}>
+            <h1 className="title">Country Quizz</h1>
+
+            {!myResult && (
+              <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} exit={{ opacity: 0 }}>
+                <Questionaire
+                  data={data}
+                  randomAnswers={randomAnswers}
+                  correctAnswerId={correctAnswerId}
+                  selectedAnswerIndex={selectedAnswerIndex}
+                  checkAnswer={checkAnswer}
+                  nextStep={nextStep}
+                  seeResult={seeResult}
+                  newQuestion={newQuestion}
+                />
+              </motion.div>
+            )}
+
+            {myResult && (
+              <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} exit={{ opacity: 0 }}>
+                <Result counter={counter} restart={restart} />
+              </motion.div>
+            )}
+          </motion.div>
           }
-        })()}
+        </AnimatePresence>
+
+
       </div>
     </main>
   );
--- src/components/Questionaire.js
+++ src/components/Questionaire.js
@@ -1,38 +1,62 @@
 import React from 'react'
 import adventure from "./../img/adventure.svg"
+import { motion, AnimatePresence } from "framer-motion"
 
-const Questionaire = ({ data, randomAnswers, correctAnswerIndex, selectedAnswerIndex, chooseAnswer, checkAnswer, nextStep, newQuestion, seeResult }) => {
+const Questionaire = ({ data, randomAnswers, correctAnswerId, selectedAnswerIndex, checkAnswer, nextStep, newQuestion, seeResult }) => {
+  const alreadyAnswered = selectedAnswerIndex !== null;
+
+  const letters = ['A', 'B', 'C', 'D'];
   return (
     <div className="box-content">
       <img className="box-content__image-decoration" src={adventure} alt="adventure" />
-      {correctAnswerIndex ? <p className="box-content__question">{data[correctAnswerIndex].capital} is the capital of</p> : ''}
-      {
-        randomAnswers.map((answer, index) => {
-          const isSelected =
-            selectedAnswerIndex !== null && index === selectedAnswerIndex;
+      {correctAnswerId ? <p className="box-content__question">{data[correctAnswerId].capital} is the capital of</p> : ''}
+      <AnimatePresence>
+        {
+          randomAnswers.map((answerId, index) => {
+            const isSelected =
+              selectedAnswerIndex !== null && index === selectedAnswerIndex;
 
-          const answeredRight = answer === correctAnswerIndex;
+            const answeredRight = answerId === correctAnswerId;
 
-          if (isSelected) {
+            if (alreadyAnswered && answeredRight) {
+              return (
+                <div
+                  className={`box-content__answer box-content__answer_good`} key={index}
+                >
+                  {letters[index]} {data[answerId].name}
+                </div>
+              );
+            }
+
+            if (isSelected) {
+              return (
+                <div
+                  className={`box-content__answer ${answeredRight ? "box-content__answer_good" : "box-content__answer_wrong"}`} key={index}
+                >
+                  {letters[index]} {data[answerId].name}
+                </div>
+              );
+            }
+
             return (
-              <div
-                className={`box-content__answer ${answeredRight ? "box-content__answer_good" : "box-content__answer_wrong"}`} key={index}
-                onClick={chooseAnswer(index)}
-              >
-                {data[answer].name}
-              </div>
+              <motion.div initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                exit={{ opacity: 0 }}
+                className="box-content__answer" key={index} onClick={() => checkAnswer(answeredRight, index)}>
+                {letters[index]} {data[answerId].name}
+              </motion.div>
             );
-          }
+          })
+        }
 
-          return (
-            <div className="box-content__answer" key={index} onClick={() => checkAnswer(answeredRight, index)}>
-              {data[answer].name}
-            </div>
-          );
-        })
-      }
-      {nextStep ? <div className="validation"><button className="button__full" onClick={newQuestion}>Next</button></div> : ''}
-      {nextStep === false ? <div className="validation"><button className="button__full" onClick={seeResult}>Sorry</button></div> : ''}
+        {nextStep ? <motion.div initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          exit={{ opacity: 0 }} className="validation"><button className="button__full" onClick={newQuestion}>Next</button></motion.div> : ''}
+        {nextStep === false ? <motion.div initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          exit={{ opacity: 0 }} className="validation"><button className="button__full" onClick={seeResult}>Sorry</button></motion.div> : ''}
+
+      </AnimatePresence>
     </div>
   );
 };
